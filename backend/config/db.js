2d8da90d734c38fe16b455b3539b13fd62d01d7b
Muhammad Saad Saif <gcsaadsaif123@gmail.com>
2026-02-17 19:39:20 +0500

    adding mongoatlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,21 @@
 const mongoose = require("mongoose");
 
-const connectDB  = async () => {
-  const mongo_URI = "mongodb://127.0.0.1:27017/user-manage-sys";
+const connectDB = async () => {
+  const mongo_URI =
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/user-manage-sys";
   try {
     await mongoose.connect(mongo_URI);
+
+    const host = mongoose.connection.host;
+    if (host.includes("mongodb.net")) {
+      console.log("connected to mongo (Atlas)");
+    } else {
+      console.log("connected to mongo (Local)");
+    }
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
 
-module.exports = connectDB ;
+module.exports = connectDB;
