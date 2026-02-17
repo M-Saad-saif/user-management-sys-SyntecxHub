9e9048d0301b402640994762acb13a8b2f741af9
Muhammad Saad Saif <gcsaadsaif123@gmail.com>
2026-02-17 22:00:32 +0500

    adding cors and dtenv

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/employees", require("./routes/employeeRoutes"));
@@ -35,7 +40,9 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
-  console.log(`user-menegement-sys server is running at http://localhost:${PORT}`);
+  console.log(
+    `user-menegement-sys server is running at http://localhost:${PORT}`,
+  );
 });
 
 // Handle unhandled promise rejections
